b0904d865d050692d75182c061e8bd1d2c9eb340
Raymond Noonan <raymond.noonan@yale.edu>
2012-06-07 00:25:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nhreads::Application.config.secret_token = '9d3666fedc469ef7980d4df5ade4e69db7a4f642cb6eb958e1ec0b8c5ce7c8faa0ee4da6b617a22a01c0a1132c1051d3cbc2e92385196c1ea355cfdb27fad069'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nhreads::Application.config.session_store :cookie_store, key: '_nhreads_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nhreads::Application.config.session_store :active_record_store
