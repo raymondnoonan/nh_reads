ec081b0cfce6094123123e5458bd5d7b1075dc75
Raymond Noonan <raymond.noonan@yale.edu>
2012-06-08 13:32:01 -0400

    Created initial User model

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class User < ActiveRecord::Base
+  # Tells rails what attributes of the model that users can modify through the user interface
+  attr_accessible :email, :name
+  before_save { |user| user.email = email.downcase }
+
+  # Validations
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+    uniqueness: { case_sensitive: false }
+end
