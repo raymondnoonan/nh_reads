ac41c02d2a176dfef05434a81011d493023501ea
Raymond Noonan <raymond.noonan@yale.edu>
2012-08-06 15:16:19 -0400

    Making User model confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :orders, dependent: :destroy
 
